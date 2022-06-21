153ac896358d477c2e839398ac5a307a820ecb9c
Emerson Millard <emillard.code@gmail.com>
2022-06-21 12:00:00 -0700

    Added Rest Template bean to main class.

--- src/main/java/com/project/MicroservicesDcb2CApplication.java
+++ src/main/java/com/project/MicroservicesDcb2CApplication.java
@@ -2,6 +2,9 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroservicesDcb2CApplication {
@@ -9,6 +12,14 @@ public class MicroservicesDcb2CApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(MicroservicesDcb2CApplication.class, args);
+
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+
+        return new RestTemplate();
 
     }
 
