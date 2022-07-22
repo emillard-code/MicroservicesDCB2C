07ada9dd649ee3b2817d25f982c435da3476e48a
Emerson Millard <emillard.code@gmail.com>
2022-07-21 17:25:16 -0700

    Initial commit

---
+++ src/main/java/com/project/MicroservicesDcb2CApplication.java
@@ -0,0 +1,15 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDcb2CApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MicroservicesDcb2CApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/project/microservicesdcb2c/MicroservicesDcb2CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.microservicesdcb2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDcb2CApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
